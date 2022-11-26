aad454db08e2e2e05710d8679ca2fbd6a5a751e6
Daani1 <danijaramillo26@yahoo.com>
2022-11-26 16:40:15 -0600

    for unicorn ride

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId:         '',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             ''          // e.g. us-east-2
+        userPoolId:         'us-east-2_XUnW4z9qd',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '8ua9411bs5pjodr39gig4gvjo',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region:             'us-east-2'          // e.g. us-east-2
     },
     api: {
         invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
